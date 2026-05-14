64d146acad876888ebe440c6cacfd139ea725e75
longinglife125-sys <longinglife125@gmail.com>
2026-05-14 11:17:08 +0800

    Update product details and add new attributes

--- products.js
+++ products.js
@@ -1,103 +1,159 @@
 const products = [
 
 {
-    id:1,
-    name:"75D 3L Waterproof Nylon Fabric",
-    category:"Waterproof",
-    gsm:"150",
-    moq:"1000m",
-    feature:"20K waterproof breathable TPU membrane",
-    image:"https://images.unsplash.com/photo-1521572267360-ee0c2909d518"
+id:1,
+name:"75D 3L Waterproof Nylon Fabric",
+category:"Waterproof",
+gsm:"150",
+width:"150cm",
+moq:"1000m",
+composition:"100% Nylon + TPU membrane",
+feature:"20K waterproof, breathable, windproof",
+application:"Outdoor jackets, hiking wear",
+image:"https://images.unsplash.com/photo-1521572267360-ee0c2909d518"
 },
 
 {
-    id:2,
-    name:"40D Ripstop Outdoor Fabric",
-    category:"Waterproof",
-    gsm:"98",
-    moq:"1200m",
-    feature:"Ultralight ripstop windproof fabric",
-    image:"https://images.unsplash.com/photo-1503341455253-b2e723bb3dbb"
+id:2,
+name:"40D Ripstop Nylon Fabric",
+category:"Waterproof",
+gsm:"95",
+width:"150cm",
+moq:"1200m",
+composition:"100% Nylon",
+feature:"Lightweight ripstop, wind resistant",
+application:"Outdoor jackets, tents",
+image:"https://images.unsplash.com/photo-1503341455253-b2e723bb3dbb"
 },
 
 {
-    id:3,
-    name:"Softshell Stretch Fabric",
-    category:"Softshell",
-    gsm:"320",
-    moq:"800m",
-    feature:"4-way stretch outdoor softshell",
-    image:"https://images.unsplash.com/photo-1503342217505-b0a15ec3261c"
+id:3,
+name:"210T Polyester Taffeta",
+category:"Waterproof",
+gsm:"75",
+width:"150cm",
+moq:"1500m",
+composition:"100% Polyester",
+feature:"Smooth surface, PU coating",
+application:"Lining, rainwear",
+image:"https://images.unsplash.com/photo-1496747611176-843222e1e57c"
 },
 
 {
-    id:4,
-    name:"TPU Laminated Rainwear Fabric",
-    category:"TPU",
-    gsm:"210",
-    moq:"500m",
-    feature:"Eco-friendly TPU laminated fabric",
-    image:"https://images.unsplash.com/photo-1515886657613-9f3515b0c78f"
+id:4,
+name:"75D Polyester Pongee",
+category:"Waterproof",
+gsm:"110",
+width:"150cm",
+moq:"1000m",
+composition:"100% Polyester",
+feature:"Soft handfeel, waterproof finish",
+application:"Jackets, outdoor wear",
+image:"https://images.unsplash.com/photo-1503342217505-b0a15ec3261c"
 },
 
 {
-    id:5,
-    name:"75D Polyester Taslon Fabric",
-    category:"Waterproof",
-    gsm:"135",
-    moq:"1000m",
-    feature:"Outdoor jacket functional fabric",
-    image:"https://images.unsplash.com/photo-1445205170230-053b83016050"
+id:5,
+name:"Nylon Taslon Fabric",
+category:"Waterproof",
+gsm:"135",
+width:"150cm",
+moq:"1200m",
+composition:"Nylon + PU coating",
+feature:"Durable, waterproof, abrasion resistant",
+application:"Workwear, outdoor jackets",
+image:"https://images.unsplash.com/photo-1445205170230-053b83016050"
 },
 
 {
-    id:6,
-    name:"4-Way Stretch Sportswear Fabric",
-    category:"Softshell",
-    gsm:"280",
-    moq:"700m",
-    feature:"Breathable activewear fabric",
-    image:"https://images.unsplash.com/photo-1529139574466-a303027c1d8b"
+id:6,
+name:"4-Way Stretch Softshell Fabric",
+category:"Softshell",
+gsm:"320",
+width:"150cm",
+moq:"800m",
+composition:"Polyester + Spandex",
+feature:"Stretchable, breathable, warm",
+application:"Hiking, ski wear",
+image:"https://images.unsplash.com/photo-1529139574466-a303027c1d8b"
 },
 
 {
-    id:7,
-    name:"Recycled Polyester Fabric",
-    category:"TPU",
-    gsm:"180",
-    moq:"900m",
-    feature:"GRS certified recycled textile",
-    image:"https://images.unsplash.com/photo-1496747611176-843222e1e57c"
+id:7,
+name:"Bonded Softshell Fabric",
+category:"Softshell",
+gsm:"340",
+width:"150cm",
+moq:"800m",
+composition:"Polyester + Fleece",
+feature:"3-layer bonded structure",
+application:"Outdoor jackets",
+image:"https://images.unsplash.com/photo-1512436991641-6745cdb1723f"
 },
 
 {
-    id:8,
-    name:"Outdoor Hiking Fabric",
-    category:"Softshell",
-    gsm:"260",
-    moq:"1000m",
-    feature:"Abrasion resistant outdoor fabric",
-    image:"https://images.unsplash.com/photo-1512436991641-6745cdb1723f"
+id:8,
+name:"Polar Fleece Softshell",
+category:"Softshell",
+gsm:"280",
+width:"150cm",
+moq:"1000m",
+composition:"100% Polyester",
+feature:"Warm, soft touch",
+application:"Winter jackets",
+image:"https://images.unsplash.com/photo-1483985988355-763728e1935b"
 },
 
 {
-    id:9,
-    name:"Waterproof Breathable Fabric",
-    category:"Waterproof",
-    gsm:"165",
-    moq:"1100m",
-    feature:"High performance breathable membrane",
-    image:"https://images.unsplash.com/photo-1483985988355-763728e1935b"
+id:9,
+name:"TPU Laminated Fabric 210D",
+category:"TPU",
+gsm:"210",
+width:"150cm",
+moq:"600m",
+composition:"Polyester + TPU",
+feature:"Waterproof + eco-friendly",
+application:"Backpacks, rainwear",
+image:"https://images.unsplash.com/photo-1523381210434-271e8be1f52b"
 },
 
 {
-    id:10,
-    name:"TPU Outdoor Bag Fabric",
-    category:"TPU",
-    gsm:"240",
-    moq:"600m",
-    feature:"Durable TPU coated fabric",
-    image:"https://images.unsplash.com/photo-1523381210434-271e8be1f52b"
+id:10,
+name:"TPU Coated Oxford Fabric",
+category:"TPU",
+gsm:"240",
+width:"150cm",
+moq:"800m",
+composition:"Polyester Oxford + TPU",
+feature:"Heavy duty waterproof fabric",
+application:"Tents, bags",
+image:"https://images.unsplash.com/photo-1515886657613-9f3515b0c78f"
+},
+
+{
+id:11,
+name:"Recycled Polyester Fabric (GRS)",
+category:"TPU",
+gsm:"160",
+width:"150cm",
+moq:"1000m",
+composition:"100% Recycled Polyester",
+feature:"Eco-friendly, GRS certified",
+application:"Sustainable garments",
+image:"https://images.unsplash.com/photo-1496747611176-843222e1e57c"
+},
+
+{
+id:12,
+name:"PU Coated Rain Fabric",
+category:"Waterproof",
+gsm:"180",
+width:"150cm",
+moq:"1000m",
+composition:"Polyester + PU",
+feature:"High waterproof performance",
+application:"Raincoats",
+image:"https://images.unsplash.com/photo-1503342217505-b0a15ec3261c"
 }
 
 ];
